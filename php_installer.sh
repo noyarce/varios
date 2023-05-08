4eac2a7448bd041a1460ee78582c911b00273605
Nicolas Oyarce V <nicolas.oyarce@mundotelecomunicaciones.cl>
2023-05-08 12:34:21 -0400

    first commit

---
+++ php_installer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt-get update
+apt-get install --no-install-recommends php8.1
+apt-get install -y php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
